87d23d083ec1cdc1f792125045078c443a3c91e9
grzechuhehe <hubertgrzego@gmail.com>
2026-05-04 15:56:43 +0200

    fixing CI

--- src/test/java/com/grzechuhehe/SportsBettingManagerApp/SportsBettingManagerAppApplicationTests.java
+++ src/test/java/com/grzechuhehe/SportsBettingManagerApp/SportsBettingManagerAppApplicationTests.java
@@ -2,8 +2,15 @@ package com.grzechuhehe.SportsBettingManagerApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "JWT_SECRET=bezpiecznyKluczTestowyJWT1234567890bezSpacjiISpecjalnychZnakow",
+    "server.ssl.enabled=false",
+    "DB_USERNAME=testuser",
+    "DB_PASSWORD=testpassword"
+})
 class SportsBettingManagerAppApplicationTests {
 
 	@Test
