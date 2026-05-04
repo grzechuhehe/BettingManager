ef8e84a1a2125752bec480f088871def8966a13c
grzechuhehe <hubertgrzego@gmail.com>
2026-05-04 17:37:14 +0200

    CI fix

--- src/test/java/com/grzechuhehe/SportsBettingManagerApp/SportsBettingManagerAppApplicationTests.java
+++ src/test/java/com/grzechuhehe/SportsBettingManagerApp/SportsBettingManagerAppApplicationTests.java
@@ -6,12 +6,7 @@ import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(properties = {
-    "JWT_SECRET=bezpiecznyKluczTestowyJWT1234567890bezSpacjiISpecjalnychZnakow",
-    "app.jwtExpirationMs=604800000",
-    "server.ssl.enabled=false",
-    "DB_USERNAME=testuser",
-    "DB_PASSWORD=testpassword",
-    "app.cors.allowed-origins=http://localhost:3000"
+    "server.ssl.enabled=false"
 })
 class SportsBettingManagerAppApplicationTests {
 
